1682528a3c97e93479f43bfbc2f932b6852ff581
truuuely <kmyc7030@gmail.com>
2025-12-29 15:56:10 +0900

    project start

---
+++ mopl-chat/src/main/java/com/mopl/MoplChatApplication.java
@@ -0,0 +1,13 @@
+package com.mopl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoplChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoplChatApplication.class, args);
+    }
+
+}

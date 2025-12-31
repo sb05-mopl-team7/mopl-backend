d92aedd6ec014965ee971d433cdc9710cb3ae0ee
Moona <moonapark1126@gmail.com>
2025-12-31 18:10:49 +0900

    [feat] 유저 이메일 여부 확인

--- mopl-api/src/main/java/com/mopl/domain/user/service/UserService.java
+++ mopl-api/src/main/java/com/mopl/domain/user/service/UserService.java
@@ -1,11 +1,8 @@
 package com.mopl.domain.user.service;
 
-import com.mopl.domain.user.entity.User;
 import com.mopl.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -13,8 +10,7 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public User findById(UUID userId) {
-        userRepository.findById(userId);
-        return null;
+    public Boolean existUser(String email) {
+        return userRepository.findByEmail(email);
     }
 }

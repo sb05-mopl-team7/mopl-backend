738bf6f7858c4a240f5156b5d75cff05b45f9ca0
Moona <moonapark1126@gmail.com>
2025-12-31 18:20:42 +0900

    [feat] 사용자 생성 dto 추가

--- mopl-api/src/main/java/com/mopl/domain/user/service/UserService.java
+++ mopl-api/src/main/java/com/mopl/domain/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.mopl.domain.user.service;
 import com.mopl.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,7 +11,8 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    @Transactional(readOnly = true)
     public Boolean existUser(String email) {
-        return userRepository.findByEmail(email);
+        return userRepository.existsByEmail(email);
     }
 }

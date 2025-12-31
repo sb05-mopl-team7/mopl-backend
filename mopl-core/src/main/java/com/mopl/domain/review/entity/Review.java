70f1f315a39022ba178108404b7cae2813b2e34c
kimyumin03 <alsfla03@gmail.com>
2025-12-31 18:23:29 +0900

    [refactor] Review 엔티티 수정

--- mopl-core/src/main/java/com/mopl/domain/review/entity/Review.java
+++ mopl-core/src/main/java/com/mopl/domain/review/entity/Review.java
@@ -1,56 +1,37 @@
 package com.mopl.domain.review.entity;
 
+import com.mopl.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Table(name = "reviews")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Review {
+public class Review extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="user_id", nullable=false)
+    @Column(name = "user_id", nullable = false)
     private Long userId;
 
-    @Column(name="content_id", nullable=false)
+    @Column(name = "content_id", nullable = false)
     private Long contentId;
 
-    @Column(name="texts", nullable=false, length=255)
+    @Column(name = "texts", nullable = false, length = 255)
     private String text;
 
-    @Column(nullable=false)
+    @Column(nullable = false)
     private double rating;
-
-    @Column(name="created_at", nullable=false, updatable=false)
-    private LocalDateTime createdAt;
-
-    @Column(name="updated_at", nullable=false)
-    private LocalDateTime updatedAt;
 
     public Review(Long userId, Long contentId, String text, double rating) {
         this.userId = userId;
         this.contentId = contentId;
         this.text = text;
         this.rating = rating;
-    }
-
-    @PrePersist
-    private void prePersist() {
-        LocalDateTime now = LocalDateTime.now();
-        this.createdAt = now;
-        this.updatedAt = now;
-    }
-
-    @PreUpdate
-    private void preUpdate() {
-        this.updatedAt = LocalDateTime.now();
     }
 }

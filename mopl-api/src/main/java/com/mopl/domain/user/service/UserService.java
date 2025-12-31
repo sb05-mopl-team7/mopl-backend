b70312f8739aec5c90a7ea7c500a538e19ce0bfb
Moona <moonapark1126@gmail.com>
2025-12-31 18:40:14 +0900

    [fix] config 기존에 이미 있는 곳으로 위치 변경

--- mopl-api/src/main/java/com/mopl/domain/user/service/UserService.java
+++ mopl-api/src/main/java/com/mopl/domain/user/service/UserService.java
@@ -1,7 +1,10 @@
 package com.mopl.domain.user.service;
 
+import com.mopl.domain.user.dto.UserCreateRequest;
+import com.mopl.domain.user.exception.UserException;
 import com.mopl.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,9 +13,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Transactional(readOnly = true)
     public Boolean existUser(String email) {
         return userRepository.existsByEmail(email);
     }
+
+    @Transactional
+    public void createUser(UserCreateRequest  userCreateRequest) {
+        if(userRepository.existsByEmail(userCreateRequest.email())){
+            throw new UserException(ErrorCode.INVALID_USER_PARAMETER);
+        }
+    }
+
+
 }

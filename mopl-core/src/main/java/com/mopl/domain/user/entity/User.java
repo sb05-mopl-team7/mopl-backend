8c82b405cc91bfe4307834e5b52e45fe6a197263
Moona <moonapark1126@gmail.com>
2025-12-31 17:51:10 +0900

    [feat] user 엔티티 추가

--- mopl-core/src/main/java/com/mopl/domain/user/entity/User.java
+++ mopl-core/src/main/java/com/mopl/domain/user/entity/User.java
@@ -1,19 +1,45 @@
 package com.mopl.domain.user.entity;
 
+import com.mopl.domain.user.enums.Role;
 import com.mopl.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.UUID;
 
 @Entity
 @Table(name = "users")
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(columnDefinition = "BINARY(16)")
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name  = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(name = "password", nullable = true, length = 255)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 10)
+    private Role role = Role.USER;
+
+    @Column (name = "locked", nullable = false)
+    private Boolean locked = false;
+
+    @Column(name = "profile_image_url", nullable = true)
+    private String profileImageUrl;
+
+    @Column(name = "provider", nullable = true)
+    private String provider;
+
+    @Column(name  = "provider_id", nullable = true)
+    private String providerId;
 }

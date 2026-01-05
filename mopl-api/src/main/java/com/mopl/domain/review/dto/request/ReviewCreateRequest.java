3ef68d57f6cd7183c3439bf6ddf42bc3a3a91707
kimyumin03 <alsfla03@gmail.com>
2026-01-05 18:27:08 +0900

    [refactor] Review request 생성 로직 수정

--- mopl-api/src/main/java/com/mopl/domain/review/dto/request/ReviewCreateRequest.java
+++ mopl-api/src/main/java/com/mopl/domain/review/dto/request/ReviewCreateRequest.java
@@ -3,14 +3,16 @@ package com.mopl.domain.review.dto.request;
 import jakarta.validation.constraints.*;
 
 public record ReviewCreateRequest(
-        @NotNull Long contentId,
+        @NotNull
+        Long contentId,
 
         @NotBlank
         @Size(max = 255)
         String text,
 
+        @NotNull
         @DecimalMin("0.0")
         @DecimalMax("5.0")
-        double rating
+        Double rating
 ) {
 }

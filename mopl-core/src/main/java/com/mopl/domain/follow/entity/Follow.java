0b64ffe4bbca07e12c57dfb3450989edd358595d
geonjineer <atoo1520@gmail.com>
2026-01-06 13:22:31 +0900

    feat: 팔로우 도메인 Entity 구현

---
+++ mopl-core/src/main/java/com/mopl/domain/follow/entity/Follow.java
@@ -0,0 +1,42 @@
+package com.mopl.domain.follow.entity;
+
+import com.mopl.domain.user.entity.User;
+import com.mopl.global.entity.BaseCreatedEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(
+    name = "follows",
+    uniqueConstraints = {
+        @UniqueConstraint(
+            name = "uk_follows_relation",
+            columnNames = {"follower_id", "followee_id"}
+        )
+    }
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Follow extends BaseCreatedEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "follower_id", nullable = false)
+  private User follower;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "followee_id", nullable = false)
+  private User followee;
+
+  @Builder
+  public Follow(User follower, User followee) {
+    this.follower = follower;
+    this.followee = followee;
+  }
+}

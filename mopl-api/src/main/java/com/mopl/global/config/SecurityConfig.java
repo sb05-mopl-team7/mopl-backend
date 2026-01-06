5d217986386f884016aee403144e39f5ff586861
truuuely <kmyc7030@gmail.com>
2026-01-06 18:49:32 +0900

    [fix] core 모듈의 yml 설정 파일을 읽지 못하는 현상 수정 - yml 이름 및 설정 변경 - H2 콘솔 접근을 위해 스프링 시큐리티 설정에 sameOrigin() 추가

--- mopl-api/src/main/java/com/mopl/global/config/SecurityConfig.java
+++ mopl-api/src/main/java/com/mopl/global/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,7 +16,7 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
-    };
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
@@ -24,15 +25,17 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                // Swagger UI
-                                "/api-docs/**",
-                                "/swagger-ui/**",
-                                "/swagger.html"
-                        ).permitAll()
-                        .anyRequest().permitAll() // ← 전체 허용(개발용)
+                                .requestMatchers(
+                                        // Swagger UI
+                                        "/api-docs/**",
+                                        "/swagger-ui/**",
+                                        "/swagger.html"
+                                ).permitAll()
+                                .anyRequest().permitAll() // ← 전체 허용(개발용)
                         //.anyRequest().authenticated()
-                );
+                )
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
 
         return http.build();
     }

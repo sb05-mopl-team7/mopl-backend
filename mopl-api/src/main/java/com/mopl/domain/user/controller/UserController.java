bbbec0ba00aac8b03b1ec310a95c4a63881b981d
Moona <moonapark1126@gmail.com>
2026-01-09 01:22:40 +0900

    [feat] 권한 제한 추가

--- mopl-api/src/main/java/com/mopl/domain/user/controller/UserController.java
+++ mopl-api/src/main/java/com/mopl/domain/user/controller/UserController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -31,6 +32,7 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/{userId}/role")
     public ResponseEntity<?>updateRole(@PathVariable Long userId, @RequestBody UserRoleUpdateRequest request){
         userService.updateRole(userId, request.role());

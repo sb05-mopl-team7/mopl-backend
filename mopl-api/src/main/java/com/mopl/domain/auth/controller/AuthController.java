f80644d80c03e32d6025f8d0c587c2f90562463f
Soyeon <isylsy166@gmail.com>
2026-01-09 14:33:16 +0900

    [feat] 비밀번호 초기화(임시 비밀번호 발급) - 의존성 추가(mail, thymeleaf) - controller - EmailService 구현 - 관련 DTO 추가

--- mopl-api/src/main/java/com/mopl/domain/auth/controller/AuthController.java
+++ mopl-api/src/main/java/com/mopl/domain/auth/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.mopl.domain.auth.controller;
 
 import com.mopl.domain.auth.dto.JwtDto;
+import com.mopl.domain.auth.dto.ResetPasswordRequest;
 import com.mopl.domain.auth.dto.SignInRequest;
 import com.mopl.domain.auth.service.AuthService;
+import com.mopl.domain.auth.service.EmailService;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final EmailService emailService;
 
     @PostMapping("/sign-in")
     public ResponseEntity<JwtDto> login(@RequestBody SignInRequest req, HttpServletResponse response) {
@@ -26,6 +30,12 @@ public class AuthController {
     @PostMapping("/sign-out")
     public ResponseEntity<?> logout(HttpServletResponse response) {
         authService.logout(response);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<Void> resetPassword(@Valid @RequestBody ResetPasswordRequest req) {
+        emailService.resetPassword(req.email());
         return ResponseEntity.ok().build();
     }
 }
---
+++ mopl-api/src/main/java/com/mopl/domain/auth/service/EmailService.java
@@ -0,0 +1,73 @@
+package com.mopl.domain.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.security.SecureRandom;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+    private final SecureRandom random = new SecureRandom();
+    private final SpringTemplateEngine templateEngine;
+
+    @Value("${spring.mail.username}")
+    private String moplEmail;
+
+    @Transactional
+    public void resetPassword(String email) {
+        // 임시 비밀번호 생성
+        String temporaryPassword = createTemporaryPassword(10);
+
+        // TODO: Redis에 저장
+
+        // 이메일 전송
+        sendEmail(email, temporaryPassword);
+
+    }
+
+    /** 이메일 전송 */
+    private void sendEmail(String email, String temporaryPassword) {
+        var message = mailSender.createMimeMessage();
+
+        try {
+            var helper = new MimeMessageHelper(message, false, "UTF-8");
+            helper.setFrom(moplEmail, "모두의 플리");
+            helper.setTo(email);
+            helper.setSubject("임시 비밀번호가 발급되었습니다.");
+
+            Context context = new Context();
+            context.setVariable("temporaryPassword", temporaryPassword);
+            String emailTemplate = templateEngine.process("reset-password", context);
+
+            helper.setText(emailTemplate, true);
+
+            mailSender.send(message);
+        } catch (Exception e) {
+            log.error("Error occurred while sending email", e);
+        }
+    }
+
+    /** 임시 비밀번호 생성 */
+    private String createTemporaryPassword(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        String PASSWORD_ALLOW_CHARS = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz23456789!@#$";
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(PASSWORD_ALLOW_CHARS.length());
+            sb.append(PASSWORD_ALLOW_CHARS.charAt(randomIndex));
+        }
+
+        return sb.toString();
+    }
+}

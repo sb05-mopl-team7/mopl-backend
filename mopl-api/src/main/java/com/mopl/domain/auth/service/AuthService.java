209b092b42288470c09ee18abaf6d59ce1426789
Moona <moonapark1126@gmail.com>
2026-01-11 20:07:48 +0900

    [refactor] 하드코딩이였던 쿠키 지속 시간을 application.yml 로 사용 하도록 함

--- mopl-api/src/main/java/com/mopl/domain/auth/service/AuthService.java
+++ mopl-api/src/main/java/com/mopl/domain/auth/service/AuthService.java
@@ -34,6 +34,12 @@ public class AuthService {
     @Value("${jwt.cookie.same-site:Lax}")  // 기본값 Lax
     private String cookieSameSite;
 
+    @Value("${jwt.access-token-validity}")
+    private int accessTokenMaxAge;
+
+    @Value("${jwt.refresh-token-validity}")
+    private int refreshTokenMaxAge;
+
     public JwtDto login(String username, String password, HttpServletResponse response) {
         User user = userRepository.findByEmail(username)
                 .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_EXIST));
@@ -46,8 +52,8 @@ public class AuthService {
 
         JwtDto jwtDto = new JwtDto(userMapper.toDto(user), accessToken);
 
-        addTokenCookie(response, "ACCESS_TOKEN", accessToken, 60 * 60); //1시간
-        addTokenCookie(response, "REFRESH_TOKEN", refreshToken, 60 * 60 * 24 * 14); //2주
+        addTokenCookie(response, "ACCESS_TOKEN", accessToken, accessTokenMaxAge); //1시간
+        addTokenCookie(response, "REFRESH_TOKEN", refreshToken, refreshTokenMaxAge); //2주
 
         return jwtDto;
     }
@@ -56,20 +62,22 @@ public class AuthService {
         deleteCookie(response, "ACCESS_TOKEN");
         deleteCookie(response, "REFRESH_TOKEN");
     }
+
     public JwtDto refresh(String refreshToken, HttpServletResponse response) {
         if (!jwtTokenProvider.validateRefreshToken(refreshToken)) {
             throw new AuthException(AuthErrorCode.REFRESH_TOKEN_INVALID);
         }
         Long userId = jwtTokenProvider.getUserId(refreshToken);
-        User user =  userRepository.findById(userId)
+        User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_EXIST));
-        String newaccessToken = jwtTokenProvider.createAccessToken(user);
-        String newrefreshToken = jwtTokenProvider.createRefreshToken(user);
-        JwtDto jwtDto = new JwtDto(userMapper.toDto(user), newaccessToken);
-        addTokenCookie(response, "ACCESS_TOKEN", newaccessToken, 60 * 60); //1시간
-        addTokenCookie(response, "REFRESH_TOKEN", newrefreshToken, 60 * 60 * 24 * 14); //2주
+        String newAccessToken = jwtTokenProvider.createAccessToken(user);
+        String newRefreshToken = jwtTokenProvider.createRefreshToken(user);
+        JwtDto jwtDto = new JwtDto(userMapper.toDto(user), newAccessToken);
 
-        return  jwtDto;
+        addTokenCookie(response, "ACCESS_TOKEN", newAccessToken, accessTokenMaxAge); //1시간
+        addTokenCookie(response, "REFRESH_TOKEN", newRefreshToken, refreshTokenMaxAge); //2주
+
+        return jwtDto;
     }
 
     private void addTokenCookie(HttpServletResponse response, String name, String value, int maxAge) {

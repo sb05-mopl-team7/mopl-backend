60446e7a402dd48af6a0fd87aac40f2322f5f9a8
Soyeon <isylsy166@gmail.com>
2026-01-12 17:05:50 +0900

    [feat] batch Controller 분리 및 TmdbClient 수정

--- mopl-batch/src/main/java/com/mopl/domain/contents/openapi/TmdbClient.java
+++ mopl-batch/src/main/java/com/mopl/domain/contents/openapi/TmdbClient.java
@@ -1,9 +1,8 @@
 package com.mopl.domain.contents.openapi;
 
-import com.mopl.domain.contents.dto.tmdb.TmdbDetailDto;
-import com.mopl.domain.contents.dto.tmdb.TmdbDto;
-import com.mopl.domain.contents.dto.tmdb.TmdbResponse;
+import com.mopl.domain.contents.dto.tmdb.*;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -29,7 +28,7 @@ public class TmdbClient {
                         .queryParam("language", "ko-KR")
                         .build())
                 .retrieve()
-                .bodyToMono(TmdbResponse.class)
+                .bodyToMono(new ParameterizedTypeReference<TmdbResponse<TmdbDto>>() {})
                 .map(response -> response.results().stream()
                         .map(TmdbDto::id)
                         .toList()
@@ -46,6 +45,38 @@ public class TmdbClient {
                         .build(movieId))
                 .retrieve()
                 .bodyToMono(TmdbDetailDto.class)
+                .retryWhen(Retry.backoff(3, Duration.ofSeconds(1)));
+    }
+
+    /** Open API에서 인기 있는 TV 시리즈 목록을 가져옵니다. */
+    public Mono<List<TvSeriesDto>> getPopularTvSeriesIdList(int page) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/tv/popular")
+                        .queryParam("page", page)
+                        .queryParam("language", "ko-KR")
+                        .build())
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<TmdbResponse<TvSeriesDto>>() {})
+                .map(response -> response.results().stream()
+                        .map(result-> {
+                            return new TvSeriesDto(result.id(), result.title(), result.description(), result.thumbnailUrl());
+                        })
+                        .toList()
+                )
+                .retryWhen(Retry.backoff(3, Duration.ofSeconds(2)));
+    }
+
+    public Mono<List<KeywordDto>> getTvSeriesKeyword(Long tvSeriesId) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/tv/{tvSeriesId}")
+                        .queryParam("append_to_response", "keywords")
+                        .queryParam("language", "ko-KR")
+                        .build(tvSeriesId))
+                .retrieve()
+                .bodyToMono(TvSeriesKeyword.class)
+                .map(TvSeriesKeyword::genres)
                 .retryWhen(Retry.backoff(3, Duration.ofSeconds(1)));
     }
 }

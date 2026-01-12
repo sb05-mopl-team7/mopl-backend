da041f4cda210c3f0af2413ea7d678aa99d8aa25
Soyeon <isylsy166@gmail.com>
2026-01-12 12:32:51 +0900

    [refactor] Redis 네임스페이스 변경

--- mopl-core/src/main/java/com/mopl/global/redis/RedisNameSpace.java
+++ mopl-core/src/main/java/com/mopl/global/redis/RedisNameSpace.java
@@ -9,13 +9,13 @@ import java.time.Duration;
 @RequiredArgsConstructor
 public enum RedisNameSpace {
     // 인증 토큰
-    AUTH_TOKEN("auth", Duration.ofDays(7), false),
+    AUTH_TOKEN("token", Duration.ofDays(7), false),
 
     // 임시 비밀번호
-    TEMP_PASSWORD("cache", Duration.ofMinutes(3), true),
+    TEMP_PASSWORD("temp-password", Duration.ofMinutes(3), true),
 
     // 실시간 시청자 수
-    WATCHER_COUNT("chat", Duration.ofHours(1), false);
+    WATCHER_COUNT("watcher", Duration.ofHours(1), false);
 
     private final String prefix;
     private final Duration ttl;

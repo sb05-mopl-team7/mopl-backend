722afdba4b7b2705308e42472ae7373526dce192
Moona <moonapark1126@gmail.com>
2026-01-14 13:19:12 +0900

    [fix] s3 연동으로 필요 없어진 파일 삭제

--- mopl-api/src/main/java/com/mopl/storage/FileStorage.java
+++
@@ -1,8 +0,0 @@
-package com.mopl.storage;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileStorage {
-    void deleteFile(String avatarImage);
-    String saveFile(MultipartFile avatarImage, long userId);
-}

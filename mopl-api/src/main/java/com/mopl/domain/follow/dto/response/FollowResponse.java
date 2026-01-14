931c264a4ffa59d13e88ee21304d54252ed72c4c
geonjineer <atoo1520@gmail.com>
2026-01-14 18:36:57 +0900

    fix : follow, watching id 필드 @JsonSerialize(using = ToStringSerializer.class) 추가

--- mopl-api/src/main/java/com/mopl/domain/follow/dto/response/FollowResponse.java
+++ mopl-api/src/main/java/com/mopl/domain/follow/dto/response/FollowResponse.java
@@ -1,8 +1,11 @@
 package com.mopl.domain.follow.dto.response;
 
 import com.mopl.domain.follow.entity.Follow;
+import tools.jackson.databind.annotation.JsonSerialize;
+import tools.jackson.databind.ser.std.ToStringSerializer;
 
 public record FollowResponse(
+        @JsonSerialize(using = ToStringSerializer.class)
         Long id,
         Long followerId,
         Long followeeId

8244debb8622d7c3908fe4ae6ed703602d1c0953
kimyumin03 <alsfla03@gmail.com>
2026-01-14 19:44:09 +0900

    [fix] ReviewDto contentId 직렬화

--- mopl-api/src/main/java/com/mopl/domain/review/dto/response/ReviewDto.java
+++ mopl-api/src/main/java/com/mopl/domain/review/dto/response/ReviewDto.java
@@ -6,6 +6,7 @@ import tools.jackson.databind.ser.std.ToStringSerializer;
 public record ReviewDto(
         @JsonSerialize(using = ToStringSerializer.class)
         Long id,
+        @JsonSerialize(using = ToStringSerializer.class)
         Long contentId,
         ReviewAuthorDto author,
         String text,

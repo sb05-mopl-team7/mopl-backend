0c34605d6a7b720e8a9aabdce0a3efc978e60792
truuuely <kmyc7030@gmail.com>
2026-01-19 00:13:46 +0900

    [feat] 권한 변경시 알림 이벤트 발행 로직 추가 - 관리자에 의해 권한 변경시 Kafka 알림 이벤트 발행 - 기존 권한과 동일하지 않은 경우에만 업데이트하도록 로직 수정

--- mopl-api/src/main/java/com/mopl/domain/user/service/UserService.java
+++ mopl-api/src/main/java/com/mopl/domain/user/service/UserService.java
@@ -1,5 +1,7 @@
 package com.mopl.domain.user.service;
 
+import com.mopl.domain.notification.enums.NotificationType;
+import com.mopl.domain.notification.event.NotificationEvent;
 import com.mopl.domain.user.dto.UserCreateRequest;
 import com.mopl.domain.user.dto.UserDto;
 import com.mopl.domain.user.dto.UserSearchCondition;
@@ -18,9 +20,11 @@ import com.mopl.global.exception.MoplException;
 import com.mopl.global.s3.FileCategory;
 import com.mopl.global.s3.S3Manager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -41,6 +45,10 @@ public class UserService {
     private final BCryptPasswordEncoder passwordEncoder;
     private final UserMapper userMapper;
     private final S3Manager s3Manager;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Value("${mopl.kafka.topics.noti}")
+    private String notificationTopic;
 
     @Transactional(readOnly = true)
     public Boolean existUser(String email) {
@@ -98,7 +106,13 @@ public class UserService {
     public void updateRole(Long userId, Role newRole) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_EXIST));
-        user.updateRole(newRole);
+
+        Role role = user.getRole();
+        if (user.updateRole(newRole)) {
+            kafkaTemplate.send(notificationTopic, NotificationEvent.of(userId,
+                    NotificationType.ROLE_UPDATED,
+                    role.name(), newRole.name()));
+        }
     }
 
     @Transactional

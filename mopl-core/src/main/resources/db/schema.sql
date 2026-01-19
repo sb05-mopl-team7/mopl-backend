4271759387659756f03f59a89f3a77f60670c505
Soyeon <isylsy166@gmail.com>
2026-01-19 13:08:30 +0900

    [chore] schema.sql 수정

--- mopl-core/src/main/resources/db/schema.sql
+++ mopl-core/src/main/resources/db/schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE contents
 (
     id             BIGINT       NOT NULL AUTO_INCREMENT,
     content_type   VARCHAR(255) NOT NULL,
-    origin_id      BIGINT       NOT NULL UNIQUE,
+    origin_id      BIGINT       NOT NULL,
     title          VARCHAR(255) NOT NULL,
     description    TEXT         NOT NULL,
     thumbnail_url  VARCHAR(255) NOT NULL,

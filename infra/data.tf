2cc44cbd6e4df990ea1d7f4e2ade9f0211412c66
Soyeon <isylsy166@gmail.com>
2026-01-22 02:39:47 +0900

    [chore] ec2 ami, type 변경

--- infra/data.tf
+++ infra/data.tf
@@ -49,3 +49,14 @@ data "aws_route53_zone" "main" {
 }
 
 data "aws_caller_identity" "current" {}
+
+# 최신 Amazon Linux 2023 AMI
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*-x86_64"]
+  }
+}
--- infra/ec2.tf
+++ infra/ec2.tf
@@ -1,7 +1,7 @@
 # EC2 Instance
 resource "aws_instance" "tools_broker" {
-  ami           = "ami-03fd85ef2fae79c05"
-  instance_type = "t3.micro"
+  ami           = data.aws_ami.amazon_linux_2023.id
+  instance_type = "t3.small"
 
   subnet_id              = aws_subnet.private_a.id
   vpc_security_group_ids = [aws_security_group.ec2.id]
@@ -10,6 +10,11 @@ resource "aws_instance" "tools_broker" {
 
   # SSM 접속용
   iam_instance_profile = aws_iam_instance_profile.tools_broker_ssm_profile.name
+
+  root_block_device {
+    volume_size = 30 # 최소 20~30GB 추천
+    volume_type = "gp3"
+  }
 
   tags = {
     Name = "${var.project_name}-ec2"

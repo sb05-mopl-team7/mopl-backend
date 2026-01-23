5c9ace3270dd410bf8454200df5c725fd5379c45
Soyeon <isylsy166@gmail.com>
2026-01-23 14:05:55 +0900

    [chore] ecs 사양 올림

--- infra/ecs-api.tf
+++ infra/ecs-api.tf
@@ -36,8 +36,8 @@ resource "aws_ecs_task_definition" "api" {
   family                   = local.api_name
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 512
-  memory                   = 1024
+  cpu                      = 1024
+  memory                   = 2048
 
   execution_role_arn = aws_iam_role.tools_broker_ssm_role.arn
   task_role_arn      = null

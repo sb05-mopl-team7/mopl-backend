1ac9708cdeac0ae53e0e8fa57d7d818157692665
Soyeon <isylsy166@gmail.com>
2026-03-17 15:55:43 +0900

    [chore] terraform: alv 도메인 이름 수정

--- infra/base/variables.tf
+++ infra/base/variables.tf
@@ -7,3 +7,8 @@ variable "project_name" {
   type    = string
   default = "mopl"
 }
+
+variable "alb_domain_name" {
+  type = string
+  default = "mopl-alb-896405909.ap-northeast-2.elb.amazonaws.com"
+}
--- infra/compute/outputs.tf
+++ infra/compute/outputs.tf
@@ -14,6 +14,10 @@ output "alb_sg_id" {
   value = aws_security_group.alb.id
 }
 
+output "alb_dns_name" {
+  value = aws_lb.this.dns_name
+}
+
 output "ecs_sg_id" {
   value = aws_security_group.ecs.id
 }
